1b6afdfafcf8cb0c7715a395faee026d87446b04
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-10-28 18:59:32 +0300

    Forward list container added

---
+++ include/alef/core/containers/forward_list.h
@@ -0,0 +1,6 @@
+#ifndef ALEF_CORE_CONTAINERS_FORWARD_LIST_H
+#define ALEF_CORE_CONTAINERS_FORWARD_LIST_H
+
+#include "alef/core/containers/forward_list/forward_list.h"
+
+#endif // ALEF_CORE_CONTAINERS_FORWARD_LIST_H
---
+++ include/alef/core/containers/forward_list/node.h
@@ -0,0 +1,24 @@
+#ifndef ALEF_CONTAINERS_FORWARD_LIST_NODE_H
+#define ALEF_CONTAINERS_FORWARD_LIST_NODE_H
+
+#include <type_traits>
+
+namespace alef::containers {
+
+template<typename T>
+struct forward_list_node {
+    T data;
+    forward_list_node* next;
+
+public:
+    forward_list_node() = default;
+
+    template<typename...Args>
+    constexpr explicit forward_list_node(Args&&...args) : data{std::forward<Args>(args)...}
+    {
+    }
+};
+
+} // namespace alef::containers::forward_list
+
+#endif // ALEF_CONTAINERS_FORWARD_LIST_NODE_H
